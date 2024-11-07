62623ddf1f3b263a66c35de3f7e0d07ddcdde6c6
Karen-404 <tat.3012.mc@gmail.com>
2024-11-07 13:28:08 -0500

    Hola

---
+++ src/main/java/ec/edu/espoch/karenmaje2026/KarenMaje2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.karenmaje2026;
+
+/**
+ *
+ * @author SO-LAB1-PC24
+ */
+public class KarenMaje2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
